16ca38d10eb13fb2cc091d34e7b50910daf466c8
Cristofer <cristoferportela@gmail.com>
2023-02-11 13:34:27 -0300

    feat: buttons working

--- src/components/PokedexDevice.vue
+++ src/components/PokedexDevice.vue
@@ -6,7 +6,7 @@
         <span></span>
       </div>
       <div class="screen">
-        <img alt="oi" :src="pokeData.sprites?.front_default" />
+        <img alt="oi" :src="picsList[pic]" />
       </div>
       <div class="screen-bottom">
         <span></span>
@@ -18,24 +18,27 @@
         </svg>
       </div>
     </section>
-    <section>
-      <div>
+    <footer class="pd-footer">
+      <div class="big-button">
         <span></span>
       </div>
-      <div>
-        <div>
+      <div class="flat-led-and-green-area">
+        <div class="flat-led">
           <span></span>
           <span></span>
         </div>
-        <span></span>
+        <span class="green-area"></span>
       </div>
-      <div>
-        <button></button>
-        <button></button>
-        <button></button>
-        <button></button>
+      <div class="direction-buttons">
+        <button @click="nextEvolution"></button>
+        <div class="horizontal">
+          <button @click="previousPic"></button>
+          <span class="central-square"></span>
+          <button @click="nextPic"></button>
+        </div>
+        <button @click="previousEvolution"></button>
       </div>
-    </section>
+    </footer>
   </article>
 </template>
 
@@ -44,7 +47,86 @@ import { Options, Vue } from 'vue-class-component';
 
 @Options({
   name: 'PokedexDevice',
-  props: ['pokeData'],
+  data() {
+    return {
+      pic: 0,
+      picsList: [],
+    };
+  },
+  beforeMount() {
+    const picsList: string[] = [];
+    Object.keys(this.pokeData.sprites).forEach((attr) => {
+      if (typeof this.pokeData.sprites[attr] === 'string') {
+        if (attr === 'front_default') {
+          picsList.unshift(this.pokeData.sprites[attr]);
+        } else {
+          picsList.push(this.pokeData.sprites[attr]);
+        }
+      }
+    });
+    this.picsList = picsList;
+  },
+  methods: {
+    previousPic: function () {
+      const pic = this.pic;
+      this.pic = pic === 0 ? this.picsList.length - 1 : pic - 1;
+    },
+    nextPic: function () {
+      console.log(this.evolutionData);
+
+      const pic = this.pic;
+      this.pic = pic === this.picsList.length - 1 ? 0 : pic + 1;
+    },
+    nextEvolution: function () {
+      if (this.evolutionData.id) {
+        let next = '';
+        const name = this.pokeData.name;
+        const chain = this.evolutionData.chain;
+
+        const isFirst = name === chain.species.name;
+        const isSecond = name === chain.evolves_to[0].species.name;
+        const isThird = name === chain.evolves_to[0].evolves_to[0].species.name;
+
+        if (isFirst) {
+          next = chain.evolves_to[0].species.name;
+        }
+        if (isSecond) {
+          next = chain.evolves_to[0].evolves_to[0].species.name;
+        }
+        if (isThird) {
+          next = chain.species.name;
+        }
+        this.$router.push('/pokemon/' + next).then(() => {
+          this.$router.go();
+        });
+      }
+    },
+    previousEvolution: function () {
+      if (this.evolutionData.id) {
+        let next = '';
+        const name = this.pokeData.name;
+        const chain = this.evolutionData.chain;
+
+        const isFirst = name === chain.species.name;
+        const isSecond = name === chain.evolves_to[0].species.name;
+        const isThird = name === chain.evolves_to[0].evolves_to[0].species.name;
+
+        if (isFirst) {
+          next = chain.evolves_to[0].evolves_to[0].species.name;
+        }
+        if (isSecond) {
+          next = chain.species.name;
+        }
+        if (isThird) {
+          next = chain.evolves_to[0].species.name;
+        }
+        this.$router.push('/pokemon/' + next).then(() => {
+          this.$router.go();
+        });
+      }
+    },
+  },
+  props: ['pokeData', 'evolutionData'],
 })
 export default class PokedexDevice extends Vue {}
 </script>
